1e22ac58725b4accfa3864c779299eb9297136b9
GreatEki <ekeneidiagbor@gmail.com>
2023-01-03 19:16:55 +0100

    chore: added skaffold

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,4 +6,4 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}!!!`));
