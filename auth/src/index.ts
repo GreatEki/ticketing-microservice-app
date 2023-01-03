fcf97c62b086dd875f91c3a501806521d49e798f
GreatEki <ekeneidiagbor@gmail.com>
2023-01-03 20:17:23 +0100

    chore: added ingress service for configuring routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,10 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("Hi there");
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}!!!`));
