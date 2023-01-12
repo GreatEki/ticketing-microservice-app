1ecb7e5bdf97842a871f1258a5f4a5a441bcfef9
GreatEki <ekeneidiagbor@gmail.com>
2023-01-12 11:46:29 +0100

    chore: defined and accessed  env variable in K8s cluster

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,10 +3,18 @@ import router from "./routes";
 import { errorHandler } from "./middlewares/error-handler";
 import { NotFoundError } from "./errors";
 import connectDB from "./config/database";
+import cookieSession from "cookie-session";
 
 const app = express();
+app.set("trust proxy", true);
 
 app.use(express.json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
 app.use("/api/users", router);
 
@@ -19,6 +27,9 @@ app.use(errorHandler);
 const PORT = process.env.PORT || 3000;
 
 const startApplication = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY should be defined");
+  }
   await connectDB();
   app.listen(PORT, () =>
     console.log(`Auth microservice listening on PORT ${PORT}!!!`)
--- auth/src/lib/signup/sign-up.controller.ts
+++ auth/src/lib/signup/sign-up.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { ApplicationError, BadRequestError } from "../../errors";
 import * as AuthService from "./sign-up.service";
+import jwt from "jsonwebtoken";
 
 export const signUpController = async (
   req: Request,
@@ -11,6 +12,17 @@ export const signUpController = async (
     const { email, password } = req.body;
 
     const signedUpUser = await AuthService.signup({ email, password });
+
+    // Generate JWT
+    const userJwt = jwt.sign(
+      { id: signedUpUser._id, email: signedUpUser.email },
+      process.env.JWT_KEY!
+    );
+
+    // Store jwt on session object
+    req.session = {
+      jwt: userJwt,
+    };
 
     return res.status(201).send({
       success: true,
