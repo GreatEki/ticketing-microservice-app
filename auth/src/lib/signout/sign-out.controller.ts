30747a10c7e4adb3f840cb5f63f8911e8af4e1f5
GreatEki <ekeneidiagbor@gmail.com>
2023-01-17 20:01:02 +0100

    feat: sign out

--- auth/src/lib/signout/sign-out.controller.ts
+++ auth/src/lib/signout/sign-out.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 
 export const signOutController = async (req: Request, res: Response) => {
-  res.send("Sign Out");
+  req.session = null;
+  res.send({});
 };
