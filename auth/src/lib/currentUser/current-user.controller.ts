cbd4006a4d69d45c41a2da050849fe38b895cc08
GreatEki <ekeneidiagbor@gmail.com>
2023-01-17 19:57:45 +0100

    feat: wip current user

--- auth/src/lib/currentUser/current-user.controller.ts
+++ auth/src/lib/currentUser/current-user.controller.ts
@@ -1,5 +1,17 @@
 import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
 
 export const currentUserController = async (req: Request, res: Response) => {
   res.send("Hi there");
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+
+    res.send({ currentUser: payload });
+  } catch (err) {
+    res.send({ currentUser: null });
+  }
 };
