6babdddd80ced1e818651afd8621ec665efc8889
GreatEki <ekeneidiagbor@gmail.com>
2023-02-04 13:09:47 +0100

    chore: tests for auth microservice

--- auth/src/lib/currentUser/__test__/current-user.test.ts
+++ auth/src/lib/currentUser/__test__/current-user.test.ts
@@ -12,3 +12,12 @@ it("returns details about the current user", async () => {
 
   expect(response.body.currentUser.email).toEqual("test@test.com");
 });
+
+it("returns null for unauthenticated users", async () => {
+  const response = await request(app)
+    .get("/api/users/currentUser")
+    .send()
+    .expect(200);
+
+  expect(response.body.currentUser).toEqual(null);
+});
