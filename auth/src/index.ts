9faf89642bf87236af3346dc6d6bdc42b1d1d5f2
GreatEki <ekeneidiagbor@gmail.com>
2023-03-27 14:43:59 +0100

    refactor: auth service MONGO_URI connection to env variable

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -7,6 +7,10 @@ const startApplication = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY should be defined");
   }
+
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI should be defined for auth service");
+  }
   await connectDB();
   app.listen(PORT, () =>
     console.log(`Auth microservice listening on PORT ${PORT}!!!`)
