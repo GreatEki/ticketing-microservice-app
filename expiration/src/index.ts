622d39c15b13040034165c2ce42cd1607a844e16
GreatEki <ekeneidiagbor@gmail.com>
2023-04-13 13:25:51 +0100

    chore: expiration service inital setup

---
+++ expiration/src/index.ts
@@ -0,0 +1,34 @@
+import { natsWrapper } from "./events/nats-wrapper";
+
+const PORT = process.env.PORT || 3000;
+
+const startApplication = async () => {
+  if (!process.env.NATS_CLUSTER_ID)
+    throw new Error("NATS CLUSTER ID env variable must be defined ");
+
+  if (!process.env.NATS_CLIENT_ID)
+    throw new Error("NATS CLIENT ID env variable must be defined ");
+
+  if (!process.env.NATS_URL)
+    throw new Error("NATS URL env variable must be defined ");
+
+  try {
+    await natsWrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed in ticket ms");
+      process.exit();
+    });
+
+    process.on("SIGINT", () => natsWrapper.client.close()); //signal interrupted
+    process.on("SIGTERM", () => natsWrapper.client.close()); //signal terminated
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+startApplication();
