d97e6d9084171356371f93a70333a36253b5469a
GreatEki <ekeneidiagbor@gmail.com>
2023-04-21 13:36:34 +0100

    feat: order model in payments ms

---
+++ payments/src/models/Order.ts
@@ -0,0 +1,53 @@
+import { OrderStatus } from "@greateki-ticket-ms-demo/common";
+import mongoose, { Schema } from "mongoose";
+import { updateIfCurrentPlugin } from "mongoose-update-if-current";
+
+interface OrderAttrs {
+  id: String;
+  version: number;
+  price: number;
+  userId: string;
+  status: OrderStatus;
+}
+
+interface OrderDoc extends mongoose.Document {
+  version: number;
+  price: number;
+  userId: string;
+  status: OrderStatus;
+}
+
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  buildNewDocument(attrs: OrderAttrs): OrderDoc;
+}
+
+const OrderSchema: Schema = new Schema<OrderAttrs>(
+  {
+    userId: { type: String, required: true },
+    price: { type: Number, required: true },
+    status: { type: String, required: true, enum: OrderStatus },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+OrderSchema.set("versionKey", "version");
+OrderSchema.plugin(updateIfCurrentPlugin);
+
+export const Order = mongoose.model<OrderDoc, OrderModel>("order", OrderSchema);
+
+OrderSchema.statics.build = (attrs: OrderAttrs) => {
+  return new Order({
+    _id: attrs.id,
+    version: attrs.version,
+    price: attrs.price,
+    userId: attrs.userId,
+    status: attrs.status,
+  });
+};
